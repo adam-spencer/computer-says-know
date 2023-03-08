fa17e0503be60e69bc9b29ea6c138d10145219b3
adam-spencer <adam.spencer2401@gmail.com>
2023-03-08 18:00:14 +0000

    Got it working

--- lifeLUCID/get_utterences.py
+++ lifeLUCID/get_utterences.py
@@ -8,13 +8,16 @@ import textgrid as tg
 import re
 import argparse
 
-END_TOKENS = {'SIL', 'SILP'} # As described in documentation
+BREAK_TOKENS = {'SILP', '<GA>'} # As described in documentation
+JUNK_TOKENS = {'SIL', '<BELL>'}
 
 def main():
   # Parse Arguments
   parser = argparse.ArgumentParser()
   parser.add_argument('in_dir', help='Name of dir to find textgrids in')
   parser.add_argument('out_dir', help='Name of dir to write JSON files to')
+  parser.add_argument('--no-normalise', '-n', action='store_true',
+                      help='Disable case normalisation')
   args = parser.parse_args()
 
   # New Path object @ specified path
@@ -23,14 +26,40 @@ def main():
     raise ValueError(f'{dir_path} is Not a directory!')
 
   files = [i for i in dir_path.iterdir() if 'Ac.TextGrid' in i.name]
-  grids = [tg.TextGrid.fromFile(f) for f in files]
+  grids = [tg.TextGrid.fromFile(f, name=f.name.removesuffix('.TextGrid'))
+           for f in files]
 
-def segment_utterances(grid:tg.TextGrid) -> dict[str, tuple]:
-  pass
+def segment_utterances(grid:tg.TextGrid, normalise:bool): #  -> dict[int, tuple]:
+  # I need to ensure this is tracking text as well as time, overlooked that!
+  segments = dict()
+  start_time = float()
+  seg_words = list()
+  seg_counter = 0
+  seg_ongoing = False
 
-def grid_to_dict(grid: tg.TextGrid) -> dict[str, tuple]:
-  pass
+  for interval in grid[0]:
+    if interval.mark in BREAK_TOKENS:
+      if seg_ongoing:
+        segments[seg_counter] = {'start' : start_time, 'end' : interval.minTime,
+                                 'words' : ' '.join(seg_words)}
+        seg_words = []
+        seg_counter += 1
+        seg_ongoing = False
+      continue
+    elif interval.mark in JUNK_TOKENS:
+      continue
+    elif not seg_ongoing:
+      start_time = interval.minTime
+      seg_ongoing = True
+    if normalise:
+      interval.mark = interval.mark.lower()
+    seg_words.append(interval.mark)
+  return segments
 
 if __name__ == '__main__':
-  main()
+  # main()
+  grid = tg.TextGrid.fromFile('/Users/adamspencer/Documents/University/third-year/diss/lifeLUCID/textgrids/NORM/OA01OA02FNORMF2_F2_Ac.TextGrid')
+  x = segment_utterances(grid, True)
+  for key, val in x.items():
+    print(val, '\n')
 
