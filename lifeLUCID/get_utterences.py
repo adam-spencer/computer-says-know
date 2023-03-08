d4f2853590a945eee1f97ec7da7b32d5cb880a88
adam-spencer <adam.spencer2401@gmail.com>
2023-03-08 16:30:11 +0000

    Added skeletal funcs

--- lifeLUCID/get_utterences.py
+++ lifeLUCID/get_utterences.py
@@ -4,19 +4,30 @@ __author__ = 'Adam Spencer'
 
 import pandas as pd
 from pathlib import Path
+import textgrid as tg
 import re
 import argparse
 
-from utils import LoadTextGrid
+def main():
+  # Parse Arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument('dirname', help='Name of dir to find textgrids in')
+  args = parser.parse_args()
 
-# Parse Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('dirname', help='Name of dir to find textgrids in')
-args = parser.parse_args()
+  # New Path object @ specified path
+  dir_path = Path(args['dirname'])
+  if not dir_path.is_dir():
+    raise ValueError(f'{dir_path} is Not a directory!')
 
-# New Path object @ specified path
-dir_path = Path(args['dirname'])
-if not dir_path.is_dir():
-  raise ValueError(f'{dir_path} is Not a directory!')
+  files = [i for i in dir_path.iterdir() if 'Ac.TextGrid' in i.name]
+  grids = [tg.TextGrid.fromFile(f) for f in files]
 
+def segment_utterances(grid:tg.TextGrid) -> dict[str, tuple]:
+  pass
+
+def grid_to_dict(grid: tg.TextGrid) -> dict[str, tuple]:
+  pass
+
+if __name__ == '__main__':
+  main()
 
