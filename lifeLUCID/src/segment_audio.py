ebe67c004a2be31e40e1f22414f5153d23a4bc70
adam-spencer <adam.spencer2401@gmail.com>
2023-03-11 17:55:01 +0000

    Minor tweaks before implementation

--- lifeLUCID/src/segment_audio.py
+++ lifeLUCID/src/segment_audio.py
@@ -11,7 +11,8 @@ import json
 import argparse
 from pathlib import Path
 
-def generate_segments(filename:str, channel:int, verbose:bool) -> np.ndarray:
+def generate_segments(filename:Path, json:Path, channel:int,
+                      verbose:bool) -> np.ndarray:
   """
 
   """
@@ -26,6 +27,8 @@ def save_segments(segs:np.ndarray, outpath:Path, verbose:bool) -> None:
 def main() -> None:
   parser = argparse.ArgumentParser()
   parser.add_argument('in_file', help='Input audio file')
+  parser.add_argument('json_file',
+                      help='Input JSON file containing segment definitions')
   parser.add_argument('out_dir', help='Output directory')
   parser.add_argument('--use-channel', '-u', choices=range(0,2),
                       help='Use a specific channel, 0 = L, 1 = R, 2 = B',
@@ -35,11 +38,12 @@ def main() -> None:
   args = parser.parse_args()
 
   in_file = Path(args.in_file)
+  json_file = Path(args.json_file)
   out_dir = Path(args.out_dir)
-  if in_file.is_dir or not out_dir.is_dir:
+  if in_file.is_dir() or json_file.is_dir() or not out_dir.is_dir():
     raise ValueError(f"Incorrect input spec!")
 
-  segments = generate_segments(in_file, args.channel, args.verbose)
+  segments = generate_segments(in_file, json_file, args.channel, args.verbose)
   save_segments(segments, out_dir, args.verbose)
 
 if __name__ == '__main__':
