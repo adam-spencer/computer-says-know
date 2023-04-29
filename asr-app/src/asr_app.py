3587c1598e49a15581df9df637cd420ee72340fb
adam-spencer <adam.spencer2401@gmail.com>
2023-04-29 15:15:59 +0100

    Reformatted files

--- asr-app/src/asr_app.py
+++ asr-app/src/asr_app.py
@@ -1,60 +1,61 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, DataTable
-from textual.widgets.data_table import Column
 import sys
 from pathlib import Path
 
 from audio_data_link import AudioDataLinker
+from textual.app import App, ComposeResult
+from textual.widgets import DataTable, Footer
 
 ROW_HEIGHT = 3
 
+
 class TranscriptionApp(App):
-  """
-  A Textual app to demonstrate semi-automatic transcription.
-  """
-  BINDINGS = [
-      ('p','play_sound','Play Audio'),
-  ]
-
-  def compose(self) -> ComposeResult:
-    """ 
-    Create child widgets for the app.
     """
-    yield DataTable(fixed_columns=1, zebra_stripes=True)
-    yield Footer()
+    A Textual app to demonstrate semi-automatic transcription.
+    """
+    BINDINGS = [
+        ('p', 'play_sound', 'Play Audio'),
+    ]
 
-  def on_mount(self) -> None:
-    table = self.query_one(DataTable)
-    rows = iter(self.data_in)
-    table.add_columns(*next(rows))
-    for row in rows:
-      table.add_row(*row, height=ROW_HEIGHT)
+    def compose(self) -> ComposeResult:
+        """ 
+        Create child widgets for the app.
+        """
+        yield DataTable(fixed_columns=1, zebra_stripes=True)
+        yield Footer()
 
-  def populate_data(self, data_linker:AudioDataLinker):
-    self.data_in = data_linker.data_for_table()
-    self.data_linker = data_linker
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        rows = iter(self.data_in)
+        table.add_columns(*next(rows))
+        for row in rows:
+            table.add_row(*row, height=ROW_HEIGHT)
 
-  def action_play_sound(self):
-    if self.selected is not None:
-      self.data_linker.play_audio(self.selected)
+    def populate_data(self, data_linker: AudioDataLinker):
+        self.data_in = data_linker.data_for_table()
+        self.data_linker = data_linker
 
-  def on_data_table_cell_highlighted(self, message: DataTable.CellHighlighted):
-    self.selected = message.coordinate.row
+    def action_play_sound(self):
+        if self.selected is not None:
+            self.data_linker.play_audio(self.selected)
+
+    def on_data_table_cell_highlighted(self, message: DataTable.CellHighlighted):
+        self.selected = message.coordinate.row
+
 
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print(f'Usage: {sys.argv[0]} path/to/audio/ path/to/data.json')
-    sys.exit(-1)
+    if len(sys.argv) != 3:
+        print(f'Usage: {sys.argv[0]} path/to/audio/ path/to/data.json')
+        sys.exit(-1)
 
-  audio_dir = Path(sys.argv[1])
-  data_file = Path(sys.argv[2])
-  if audio_dir.is_dir() and data_file.exists():
-    data_linker = AudioDataLinker(audio_dir, data_file)
-  else:
-    print('Something is wrong with your input!')
-    print(f'I got {audio_dir} and {data_file}')
-    sys.exit(-1)
+    audio_dir = Path(sys.argv[1])
+    data_file = Path(sys.argv[2])
+    if audio_dir.is_dir() and data_file.exists():
+        data_linker = AudioDataLinker(audio_dir, data_file)
+    else:
+        print('Something is wrong with your input!')
+        print(f'I got {audio_dir} and {data_file}')
+        sys.exit(-1)
 
-  app = TranscriptionApp()
-  app.populate_data(data_linker)
-  app.run()
+    app = TranscriptionApp()
+    app.populate_data(data_linker)
+    app.run()
--- asr-app/src/audio_data_link.py
+++ asr-app/src/audio_data_link.py
@@ -1,56 +1,58 @@
-import textwrap
-import soundfile as sf
-import sounddevice as sd
-import pandas as pd
-import numpy as np
 import json
+import textwrap
 from pathlib import Path
+
+import sounddevice as sd
+import soundfile as sf
 
 WRAP_WIDTH = 40
 
+
 class AudioDataLinker:
-  """ 
-  Object to aid linking audio and transcript data
-  """
-  def __init__(self, audio_dir:Path, data_file:Path) -> None:
     """ 
-    Create a new AudioDataLinker.
-
-    :param audio_dir: Directory within which audio files are stored.
-    :param data_file: File containing transcription data.
+    Object to aid linking audio and transcript data
     """
-    with open(data_file) as f:
-      self.data = json.load(f)
-    self.audio_dir = audio_dir
 
-  def play_audio(self, idx:int) -> bool:
-    """ 
-    Play audio correlated with an utterance.
+    def __init__(self, audio_dir: Path, data_file: Path) -> None:
+        """ 
+        Create a new AudioDataLinker.
 
-    :param idx: Index of utterance.
-    :returns: True if audio file exists, false otherwise.
+        :param audio_dir: Directory within which audio files are stored.
+        :param data_file: File containing transcription data.
+        """
+        with open(data_file) as f:
+            self.data = json.load(f)
+        self.audio_dir = audio_dir
+
+    def play_audio(self, idx: int) -> bool:
+        """ 
+        Play audio correlated with an utterance.
+
+        :param idx: Index of utterance.
+        :returns: True if audio file exists, false otherwise.
+        """
+        audio_file = self.audio_dir / f'{idx:03d}.wav'
+        if not audio_file.exists():
+            return False
+        sd.play(*sf.read(audio_file), blocking=True)
+
+    def data_for_table(self, height: int = 2) -> list:
+        """ 
+        Get data in format required to be printed in DataTable object.
+
+        :returns: List containing table rows.
+        """
+        table_rows = [('ID', 'Hypothesis', 'Reference')]
+        for idx, utterance in self.data.items():
+            ref = wrap(utterance['transcript'])
+            hyp = wrap(utterance['whisper']['text'])
+            table_rows.append(
+                (idx, hyp, ref))
+        return table_rows
+
+
+def wrap(string: str) -> str:
     """
-    audio_file = self.audio_dir / f'{idx:03d}.wav'
-    if not audio_file.exists():
-      return False
-    sd.play(*sf.read(audio_file), blocking=True)
-
-  def data_for_table(self, height:int=2) -> list:
-    """ 
-    Get data in format required to be printed in DataTable object.
-
-    :returns: List containing table rows.
+    Apply text wrapping to a string.
     """
-    table_rows = [('ID', 'Hypothesis', 'Reference')]
-    for idx, utterance in self.data.items():
-      ref = wrap(utterance['transcript'])
-      hyp = wrap(utterance['whisper']['text'])
-      table_rows.append(
-          (idx, hyp, ref))
-    return table_rows
-
-def wrap(string:str) -> str:
-  """
-  Apply text wrapping to a string.
-  """
-  return '\n'.join(textwrap.wrap(string, width=WRAP_WIDTH))
+    return '\n'.join(textwrap.wrap(string, width=WRAP_WIDTH))
