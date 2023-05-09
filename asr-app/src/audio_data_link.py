b262fdf3eb6411833f399918566678c5e0a19e6f
adam-spencer <adam.spencer2401@gmail.com>
2023-05-09 16:37:19 +0100

    Added docstrings

--- asr-app/src/audio_data_link.py
+++ asr-app/src/audio_data_link.py
@@ -51,10 +51,6 @@ class AudioDataLinker:
             kwgs['ref'] = wrap(utterance['transcript'])
             kwgs['wer'] = utterance['wer']
             if self.confidence_mode:
-                # TODO:
-                #  * Do some text highlighting thing?
-                #  * Blanking out of text below threshold?
-                #  * (in other file) sort on confidence measures
                 kwgs['conf_scoring'] = utterance['confidence_scoring']
             else:
                 kwgs['avg_logprob'] = utterance['avg_logprob']
@@ -67,9 +63,27 @@ class AudioDataLinker:
         table_rows.insert(0, self.row_data[0].get_header())
         return table_rows
 
+        # TODO:
+        #  * Do some text highlighting thing?
+        #  * Blanking out of text below threshold?
+        #  * (in other file) sort on confidence measures
+
 
 class TableRow:
+    """
+    Object to represent table row.
+
+    Using OOP allows text manipulation.
+    """
+
     def __init__(self, confidence_mode: bool, **kwargs):
+        """
+        Create a new TableRow.
+
+        :param confidence_mode: True if using confidence mode.
+        :param kwargs: Data to insert into table. 
+            `kwargs` allows different options based on confidence mode.
+        """
         self.confidence_mode = confidence_mode
         self.idx = int(kwargs['idx'])
         self.hyp = kwargs['hyp']
@@ -87,17 +101,30 @@ class TableRow:
             self.avg_logprob = kwargs['avg_logprob']
 
     def for_table(self):
+        """Get row data in correct format for insertion into table"""
         if self.confidence_mode:
             return (int(self.idx), self.hyp, self.ref, float(self.utt_conf),
                     float(self.max_conf), float(self.min_conf), float(self.wer))
+        else:
+            (int(self.idx), self.hyp, self.ref, float(
+                self.avg_logprob), float(self.wer))
 
     def get_header(self):
+        """Get header row for table"""
         if self.confidence_mode:
             return [('ID', 'Hypothesis', 'Reference',
                            'Utterance Confidence', 'Max Conf.', 'Min Conf.', 'WER')]
         else:
             return [('ID', 'Hypothesis', 'Reference',
                            'Average Log Probability', 'WER')]
+
+    def text_highlighting(self):
+        """Highlight text based on confidence measure"""
+        pass
+
+    def text_blanking(self, threshold: float):
+        """Blank out text based on confidence threshold"""
+        pass
 
 
 def wrap(string: str) -> str:
