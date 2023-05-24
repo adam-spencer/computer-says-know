2fe213ee2bd14c0707885f961a1b4cc735bac12a
adam-spencer <adam.spencer2401@gmail.com>
2023-05-24 13:41:22 +0100

    More code, more implemetnation bit

---
+++ report/diss-final/code/asr_app.py
@@ -0,0 +1,157 @@
+import cmd
+import shutil
+from pathlib import Path
+
+from audio_data_link import AudioDataLinker
+from textual.app import App, ComposeResult
+from textual.coordinate import Coordinate
+from textual.widgets import DataTable, Footer
+
+ROW_HEIGHT = 4
+
+
+class TranscriptionApp(App):
+    """
+    A Textual app to demonstrate semi-automatic transcription.
+    """
+    BINDINGS = [
+        ('p', 'play_sound', 'Play Audio'),
+        ('s', 'sort_id', 'Sort ID'),
+        ('w', 'sort_wer', 'Sort WER'),
+        ('u', 'sort_utt_conf', 'Sort Utterance Conf'),
+        ('>', 'sort_max_conf', 'Sort Max Conf.'),
+        ('<', 'sort_min_conf', 'Sort Min Conf.'),
+        ('a', 'sort_prob', 'Sort Av. Log Prob.'),
+    ]
+
+    def compose(self) -> ComposeResult:
+        """ 
+        Create child widgets for the app.
+        """
+        yield DataTable(fixed_columns=1, zebra_stripes=True)
+        yield Footer()
+
+    def on_mount(self) -> None:
+        """This is run upon starting the App."""
+        self.current_sort_col = 'ID'
+        self.reversed_sort = False
+        table = self.query_one(DataTable)
+        rows = iter(self.data_in)
+        for col_name in next(rows):
+            table.add_column(col_name, key=col_name)
+        for row in rows:
+            table.add_row(*row, height=ROW_HEIGHT)
+
+    def populate_data(self, data_linker: AudioDataLinker):
+        """Populate table data."""
+        self.data_in = data_linker.data_for_table()
+        self.data_linker = data_linker
+
+    def action_play_sound(self):
+        """Play the utterance."""
+        if self.selected_idx is not None:
+            self.data_linker.play_audio(self.selected_idx)
+
+    def sort_fn(self, col: str):
+        """Function to sort a given column."""
+        rev = False
+        if self.current_sort_col == col and not self.reversed_sort:
+            rev = True
+        self.current_sort_col = col
+        self.reversed_sort = rev
+        dt = self.query_one(DataTable)
+        dt.sort(col, reverse=rev)
+
+    def action_sort_id(self):
+        self.sort_fn('ID')
+
+    def action_sort_wer(self):
+        self.sort_fn('WER')
+
+    def action_sort_prob(self):
+        if not self.data_linker.confidence_mode:
+            self.sort_fn('Average Log Probability')
+
+    def action_sort_utt_conf(self):
+        if self.data_linker.confidence_mode:
+            self.sort_fn('Utterance Confidence')
+
+    def action_sort_max_conf(self):
+        if self.data_linker.confidence_mode:
+            self.sort_fn('Max Conf.')
+
+    def action_sort_min_conf(self):
+        if self.data_linker.confidence_mode:
+            self.sort_fn('Min Conf.')
+
+    def on_data_table_cell_highlighted(self, message: DataTable.CellHighlighted):
+        """Keeps track of current highlighted cell to enable playing of sound."""
+        row_num = message.coordinate.row
+        idx_coord = Coordinate(row_num, 0)
+        self.selected_idx = self.query_one(DataTable).get_cell_at(idx_coord)
+
+
+def launcher() -> (Path, Path, bool, bool, bool):
+    """
+    Launcher for my ASR App!
+
+    In order for this to work, asr_app.py must be run from the `asr-app/src`
+    directory.
+
+    It will open up a list of conversatiosn to choose from, take a user's input
+    as a number corresponding to a conversation and then the app will launch.
+
+    :returns: something...
+    """
+    print("Would you like to use data with confidence scores? [y/n]")
+    use_confidence_inp = input("\n-> ")
+    use_confidence = False
+    text_highlight = False
+    text_blanking = False
+    blanking_threshold = 0.0
+    if use_confidence_inp.lower() == 'y':
+        use_confidence = True
+        print('\nWould you like to use text blanking, highlighting, or neither?'
+              + '[b/h/n]\n')
+        conf_opt = input('-> ').lower()
+        if conf_opt == 'b':
+            text_blanking = True
+            blanking_threshold = float(input('Threshold : '))
+        elif conf_opt == 'h':
+            text_highlight = True
+
+    data_path = Path('../data')
+    if use_confidence:
+        asr_data_path = data_path / 'confidence-data'
+    else:
+        asr_data_path = data_path / 'asr-out'
+    audio_dirs_list = sorted(
+        [i for i in (data_path / 'audio').iterdir() if i.is_dir()])
+
+    # Present the conversations by code and allow the user to choose
+    convos = [f'{idx:2} : {d.name} ' for idx, d in enumerate(audio_dirs_list)]
+    cli = cmd.Cmd()
+    print('\nChoose a conversation:\n')
+    cli.columnize(convos, displaywidth=shutil.get_terminal_size().columns)
+    chosen_audio = audio_dirs_list[int(input('\n-> '))]
+
+    # Find the data file corresponding to the chosen conversation
+    chosen_data = None
+    for f in asr_data_path.iterdir():
+        if chosen_audio.name in f.name:
+            chosen_data = f
+    if chosen_data:
+        return (chosen_data, chosen_audio, use_confidence,
+                text_blanking, blanking_threshold, text_highlight)
+    raise IndexError('Can\'t find data for selected conversation')
+
+
+if __name__ == "__main__":
+    (data_file, audio_dir, confidence_mode, text_blanking,
+     blanking_threshold, text_highlight) = launcher()
+    data_linker = AudioDataLinker(
+        audio_dir, data_file, confidence_mode, text_blanking, blanking_threshold,
+        text_highlight)
+    app = TranscriptionApp()
+    app.populate_data(data_linker)
+    app.run()
---
+++ report/diss-final/code/audio_data_link.py
@@ -0,0 +1,207 @@
+import json
+import sys
+import textwrap
+from pathlib import Path
+
+import sounddevice as sd
+import soundfile as sf
+from rich.color import Color
+from rich.style import Style
+from rich.text import Text
+
+WRAP_WIDTH = 40
+BLOCK = '\u2588'  # Unicode block character for blanking out words
+
+
+class AudioDataLinker:
+    """
+    Object to aid linking audio and transcript data
+    """
+
+    def __init__(self, audio_dir: Path, data_file: Path, confidence_mode: bool,
+                 text_blanking: bool = False, blanking_threshold: float = 0.5,
+                 text_highlight: bool = False) -> None:
+        """
+        Create a new AudioDataLinker.
+
+        :param audio_dir: Directory within which audio files are stored.
+        :param data_file: File containing transcription data.
+        :param confidence_mode: True if confidence scores are being used.
+        :param text_blanking: True to enable text blanking mode.
+        :param text_highlight: True to enable text highlighting based on confidence.
+        """
+        with open(data_file) as f:
+            self.data = json.load(f)
+        self.audio_dir = audio_dir
+        self.confidence_mode = confidence_mode
+        if text_blanking or text_highlight:
+            if text_blanking and text_highlight:
+                print('Error! Can\'t blank and highlight at the same time :<')
+                sys.exit(1)
+        self.text_blanking = text_blanking
+        self.blanking_threshold = blanking_threshold
+        self.text_highlight = text_highlight
+        self.row_data = self.init_row_data()
+
+    def play_audio(self, idx: int) -> bool:
+        """
+        Play audio correlated with an utterance.
+
+        :param idx: Index of utterance.
+        :returns: True if audio file exists, false otherwise.
+        """
+        audio_file = self.audio_dir / f'{idx:03d}.wav'
+        if not audio_file.exists():
+            return False
+        sd.play(*sf.read(audio_file))
+
+    def init_row_data(self) -> list:
+        """
+        Get data in format required to be printed in DataTable object.
+
+        :returns: List containing table rows.
+        """
+        row_data = []
+        for idx, utterance in self.data.items():
+            kwgs = dict()
+            kwgs['idx'] = idx
+            kwgs['hyp'] = wrap_and_format(utterance['whisper']['text'])
+            kwgs['ref'] = wrap_and_format(utterance['transcript'])
+            kwgs['wer'] = utterance['wer']
+            if self.confidence_mode:
+                if conf_scoring := utterance.get('confidence_scoring'):
+                    kwgs['conf_scoring'] = conf_scoring
+                else:
+                    continue
+            else:
+                kwgs['avg_logprob'] = utterance['avg_logprob']
+            row = TableRow(self.confidence_mode,
+                           self.text_blanking, self.text_highlight,
+                           self.blanking_threshold, **kwgs)
+            row_data.append(row)
+        return row_data
+
+    def data_for_table(self) -> list:
+        table_rows = [row.for_table() for row in self.row_data]
+        table_rows.insert(0, self.row_data[0].get_header())
+        return table_rows
+
+
+class TableRow:
+    """
+    Object to represent table row.
+
+    Using OOP simplifies text manipulation in table.
+    """
+
+    def __init__(self, confidence_mode: bool, text_blanking: bool = False,
+                 text_highlight: bool = False, blanking_threshold: float = 0.5,
+                 **kwargs):
+        """
+        Create a new TableRow.
+
+        :param confidence_mode: True if using confidence mode.
+        :param text_blanking: True to enable text blanking.
+        :param text_highlight: True to enable text highlighting.
+        :param blanking_threshold: Set the confidence threshold to blank text.
+        :param kwargs: Data to insert into table. 
+            `kwargs` allows different options based on confidence mode.
+        """
+        self.confidence_mode = confidence_mode
+        self.highlight_mode = text_highlight
+        self.idx = int(kwargs['idx'])
+        self.hyp = kwargs['hyp']
+        self.ref = kwargs['ref']
+        self.wer = kwargs['wer']
+        if confidence_mode:
+            conf_scoring = kwargs['conf_scoring']
+            scores = conf_scoring['confidence_scores']
+            self.token_conf_pair = list(zip(
+                conf_scoring['words'].split(), scores))
+            if len(scores) > 0:
+                self.max_conf = max(scores)
+                self.min_conf = min(scores)
+            else:
+                self.max_conf = 0.0
+                self.min_conf = 0.0
+            self.utt_conf = conf_scoring['utterance_confidence']
+        else:
+            self.avg_logprob = kwargs['avg_logprob']
+
+        if text_blanking:
+            self.hyp_to_print = wrap_and_format(
+                self.text_blanking(blanking_threshold))
+        elif text_highlight:
+            self.hyp_to_print = self.text_highlighting()
+        else:
+            self.hyp_to_print = wrap_and_format(self.hyp)
+
+    def for_table(self):
+        """Get row data in correct format for insertion into table."""
+        if self.confidence_mode:
+            return (int(self.idx), self.hyp_to_print, self.ref,
+                    float(self.utt_conf), float(self.max_conf),
+                    float(self.min_conf), float(self.wer))
+        else:
+            (int(self.idx), self.hyp_to_print, self.ref, float(
+                self.avg_logprob), float(self.wer))
+
+    def get_header(self):
+        """Get header row for table."""
+        if self.confidence_mode:
+            return ['ID', 'Hypothesis', 'Reference',
+                    'Utterance Confidence', 'Max Conf.', 'Min Conf.', 'WER']
+        else:
+            return ['ID', 'Hypothesis', 'Reference',
+                    'Average Log Probability', 'WER']
+
+    def text_highlighting(self) -> Text:
+        """
+        Highlight text based on confidence measure.
+
+        :returns: Text with colour between red and green.
+        """
+        text_list = []
+        for word, conf in self.token_conf_pair:
+            text_style = compute_colour(conf)
+            # Highlight text and replace underscores with space
+            text_obj = Text(word.replace('_', ' '), style=text_style)
+            text_list.append(text_obj)
+        # Wrap text to be printed properly
+        lines = Text(' ').join(text_list).wrap(None, WRAP_WIDTH)
+        return Text('\n').join(lines)
+
+    def text_blanking(self, threshold: float) -> str:
+        """Blank out text based on confidence threshold."""
+        text_list = []
+        for word, conf in self.token_conf_pair:
+            if conf < threshold:
+                # Removes underscores when printing blocks
+                for subword in word.split('_'):
+                    text_list.append(len(subword) * BLOCK)
+            else:
+                text_list.append(word)
+        return ' '.join(text_list)
+
+
+def compute_colour(conf: float) -> Style:
+    """
+    Compute a colour between red and green for highlighting text.
+
+    :param conf: Confidence score between 0 and 1.
+    :returns: Style to highlight text with.
+    """
+    r1, g1, b1 = (255, 0, 0)  # red
+    r2, g2, b2 = (0, 255, 0)  # green
+    rn, gn, bn = (
+        r1 + conf * (r2 - r1),
+        g1 + conf * (g2 - g1),
+        b1 + conf * (b2 - b1)
+    )
+    return Style(color=Color.from_rgb(rn, gn, bn))
+
+
+def wrap_and_format(string: str) -> str:
+    """Apply text wrapping to a string and remove underscores."""
+    string_no_uscore = string.replace('_', ' ')
+    return '\n'.join(textwrap.wrap(string_no_uscore, width=WRAP_WIDTH))
